9fa985404de4b66984f63d45827c4375997345c4
Connykrautstingl <s44784@edu.campus02.at>
2021-10-09 10:44:19 +0200

    Turmrechnen start

---
+++ src/Turm.java
@@ -0,0 +1,14 @@
+public class Turm {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Turm aufsteigend: " +i);
+        }
+
+        for (int j = 10; j >= 1; j--){
+            System.out.println("Turm Absteigend: " + j);
+        }
+
+    }
+}
